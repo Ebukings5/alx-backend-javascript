be45ec9beb8166d33f06c257076c3d092c5eb521
Ebukings5 <cristofarokoye@gmail.com>
2024-10-03 17:55:39 +0100

    ES6 classes

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -1,15 +1,19 @@
 export default class HolbertonClass {
     constructor(year, location) {
-        this.year = year;
-        this.location = location;
+        this._year = year;       // Set private variable _year
+        this._location = location; // Set private variable _location
     }
+
     get year() {
         return this._year;
     }
+
     get location() {
         return this._location;
     }
 }
+
+// Create instances of HolbertonClass
 const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
 
@@ -19,23 +23,30 @@ export class StudentHolberton {
         this._lastName = lastName;
         this._holbertonClass = holbertonClass;
     }
+
     get fullStudentDescription() {
         return `${this._firstName} ${this._lastName} is a student of the Holberton Class ${this._holbertonClass.year}`;
     }
+
     get fullName() {
         return `${this._firstName} ${this._lastName}`;
     }
+
     get holbertonClass() {
         return this._holbertonClass;
     }
+
     get fullClassDescription() {
-        return `${this._firstName} - ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
+        return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
     }
 }
+
+// Create instances of StudentHolberton
 const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
 const student2 = new StudentHolberton('John', 'Doe', class2020);
 const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
 const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
+// Export the list of students
 export const listOfStudents = [student1, student2, student3, student4, student5];
