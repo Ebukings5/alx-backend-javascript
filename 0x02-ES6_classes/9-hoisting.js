7bb1f7a84ac7c3be73382d28a0549643b6a97e6d
Ebukings5 <cristofarokoye@gmail.com>
2024-10-03 17:47:16 +0100

    ES6 classes

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -34,8 +34,8 @@ export class StudentHolberton {
 }
 const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
 const student2 = new StudentHolberton('John', 'Doe', class2020);
-const student3 = new StudentHolberton('Albert', 'Einstein', class2019);
-const student4 = new StudentHolberton('Donald', 'Trump', class2019);
-const student5 = new StudentHolberton('Barack', 'Obama', class2019);
+const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
+const student4 = new StudentHolberton('Donald', 'Bush', class2019);
+const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
 export const listOfStudents = [student1, student2, student3, student4, student5];
